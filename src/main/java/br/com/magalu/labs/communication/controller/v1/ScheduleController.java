4e841d9057239e0f07449e6ca181e5065fab488d
maurigre@gmail.com <7UUjepGDa4yquMN__!@#$4321>
2021-04-12 04:01:03 -0300

    feat(Controller): incluido a injecao de dependencia do service de mensagem e rabbitmq

--- src/main/java/br/com/magalu/labs/communication/controller/v1/ScheduleController.java
+++ src/main/java/br/com/magalu/labs/communication/controller/v1/ScheduleController.java
@@ -1,14 +1,21 @@
 package br.com.magalu.labs.communication.controller.v1;
 
+import br.com.magalu.labs.communication.core.service.message.MessageService;
+import br.com.magalu.labs.communication.service.rabbitmq.RabbitMqService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 @RequestMapping(ScheduleController.PATH)
 public class ScheduleController {
 
     static final String VERSION = "/v1";
     static final String PATH = VERSION + "/schedules";
 
+    MessageService messageService;
+    RabbitMqService rabbitMqService;
 
     @GetMapping
     public String findAllSchedules() {

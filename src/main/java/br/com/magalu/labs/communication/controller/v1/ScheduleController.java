35f997e3451bc243e9ca7f58ca458f64a28c44b5
maurigre@gmail.com <7UUjepGDa4yquMN__!@#$4321>
2021-04-13 03:40:56 -0300

    feat(): realizado a implementacao dos metodos do controller

--- src/main/java/br/com/magalu/labs/communication/controller/v1/ScheduleController.java
+++ src/main/java/br/com/magalu/labs/communication/controller/v1/ScheduleController.java
@@ -1,10 +1,28 @@
 package br.com.magalu.labs.communication.controller.v1;
 
+import br.com.magalu.labs.communication.controller.v1.dto.mapper.MessageMapper;
+import br.com.magalu.labs.communication.controller.v1.dto.message.MessageDto;
+import br.com.magalu.labs.communication.controller.v1.dto.response.Response;
+import br.com.magalu.labs.communication.core.model.Message;
 import br.com.magalu.labs.communication.core.service.message.MessageService;
 import br.com.magalu.labs.communication.service.rabbitmq.RabbitMqService;
+import br.com.magalu.labs.communication.validations.MessageValidation;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static br.com.magalu.labs.communication.controller.v1.dto.mapper.MessageMapper.DtoToMessage;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 
 @RestController
 @AllArgsConstructor(onConstructor = @__(@Autowired))
@@ -17,27 +35,56 @@ public class ScheduleController {
     private final MessageService messageService;
     private final RabbitMqService rabbitMqService;
 
-    @GetMapping
-    public String findAllSchedules() {
-        return "Ola mundo";
+    @GetMapping(produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<Response<List<MessageDto>>> findAllSchedules() {
+        List<MessageDto> messageDtos = new ArrayList<>();
+        messageService.findAll().stream()
+                .forEach(message -> {
+                    MessageDto dto = MessageMapper.messageToDto(message);
+                    dto.add(linkTo(methodOn(ScheduleController.class).findById(message.getId()))
+                                    .withSelfRel().expand());
+                    messageDtos.add(dto);
+                });
+        Response<List<MessageDto>> response = new Response<>();
+        response.setData(messageDtos);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public void findById(@PathVariable Long id) {
-
+    @GetMapping(produces = APPLICATION_JSON_VALUE, value = "/{id}")
+    public ResponseEntity<Response<MessageDto>> findById(@PathVariable Long id) {
+        Optional<Message> messageOptional = messageService.findById(id);
+        if (!messageOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Response<MessageDto> response = new Response<>();
+        response.setData(MessageMapper.messageToDto(messageOptional.get()));
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @PostMapping
-    public void create() {
+    @PostMapping(produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object>  create(@RequestBody @Valid MessageDto messageDto) {
+        Response<MessageDto> response = new Response<>();
+        final MessageValidation messageValidation = new MessageValidation(messageDto);
+        if (!messageValidation.haveNoErrors()) {
+            return new ResponseEntity<>(messageValidation.getErrorValid(), HttpStatus.BAD_REQUEST);
+        }
+        final Message message = messageService.save(DtoToMessage(messageDto)).get();
+        MessageDto dto = MessageMapper.messageToDto(message);
+        dto.add(linkTo(methodOn(ScheduleController.class).findById(message.getId())).withSelfRel().expand());
+
+        response.setData(dto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
-    }
-
-    @PutMapping
-    public void update() {
-
+    public ResponseEntity<Response<?>> delete(@PathVariable Long id) {
+        Response<?> response = new Response<>();
+        final Optional<Message> messageOptional = messageService.findById(id);
+        if (!messageOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        messageService.deleteById(messageOptional.get().getId());
+        return new ResponseEntity<>(response, HttpStatus.NO_CONTENT);
     }
 
 }
--- src/main/java/br/com/magalu/labs/communication/controller/v1/dto/message/MessageDto.java
+++ src/main/java/br/com/magalu/labs/communication/controller/v1/dto/message/MessageDto.java
@@ -1,17 +1,32 @@
-package br.com.magalu.labs.communication.controller.v1.dto;
+package br.com.magalu.labs.communication.controller.v1.dto.message;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import lombok.Builder;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.hateoas.RepresentationModel;
 
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
+import java.util.Calendar;
 
 @Data
+@Builder
 public class MessageDto extends RepresentationModel<MessageDto> {
 
     @NotNull(message = "Data hora não pode ser null")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "UTC-3")
+    @JsonSerialize(using = ToStringSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS", locale = "en-US", timezone = "Brazil/East")
+   // @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "UTC-3")
     private LocalDateTime dateTime;
 
     @NotNull(message = "Destino não pode ser null")
@@ -21,9 +36,10 @@ public class MessageDto extends RepresentationModel<MessageDto> {
     private String message;
 
     @NotNull(message = "Tipo da mensagem não pode ser null")
+    @Pattern(regexp="^(EMAIL|SMS|PUSH|WHATSAPP)$",
+            message="Para o type apenas os valores EMAIL, SMS, PUSH e WHATSAPP sao aceitos.")
     private String type;
 
-    private String status;
-
+    private String state;
 
 }

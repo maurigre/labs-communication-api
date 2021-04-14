0d952bdff6de6049d0e4916aa5ade548779a803e
maurigre@gmail.com <7UUjepGDa4yquMN__!@#$4321>
2021-04-14 04:02:03 -0300

    doc(): melhorando a documentacao da API com swagger

--- src/main/java/br/com/magalu/labs/communication/controller/v1/ScheduleController.java
+++ src/main/java/br/com/magalu/labs/communication/controller/v1/ScheduleController.java
@@ -6,6 +6,10 @@ import br.com.magalu.labs.communication.controller.v1.dto.response.Response;
 import br.com.magalu.labs.communication.core.model.Message;
 import br.com.magalu.labs.communication.core.model.MessageState;
 import br.com.magalu.labs.communication.core.service.message.MessageService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,9 +26,10 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 
-@RestController
+@RestController("ScheduleController")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 @RequestMapping(ScheduleController.PATH)
+@Api(value = "Labs Communication API", tags = "Labs Communication API")
 public class ScheduleController {
 
     static final String VERSION = "/v1";
@@ -32,6 +37,10 @@ public class ScheduleController {
 
     private final MessageService messageService;
 
+    @ApiOperation(value = "List all scheduled messages")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "List scheduled messages sucessFully")
+    })
     @GetMapping(produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<Response<List<MessageDto>>> findAllSchedules() {
         List<MessageDto> messageDtos = new ArrayList<>();

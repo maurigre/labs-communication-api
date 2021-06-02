2653a10a3830105c86cc78612ea4e835cb35804b
Mauri <maurigre@gmail.com>
2021-06-02 18:03:06 -0300

    test(): starting creation of unit tests for rabbitmq

--- src/test/java/br/com/magalu/labs/communication/service/rabbitmq/RabbitMqServiceUnitTest.java
+++ src/test/java/br/com/magalu/labs/communication/service/rabbitmq/RabbitMqServiceUnitTest.java
@@ -1,5 +1,27 @@
 package br.com.magalu.labs.communication.service.rabbitmq;
 
-public interface RabbitMqServiceTest {
-    void producer(Object mensagem);
+import br.com.magalu.labs.communication.service.rabbitmq.imp.RabbitMqServiceImp;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+import static org.mockito.Mockito.mock;
+
+class RabbitMqServiceUnitTest {
+
+    RabbitTemplate rabbitTemplate;
+    RabbitMqService rabbitMqService;
+
+    @BeforeEach
+    void setUp() {
+        this.rabbitTemplate = mock(RabbitTemplate.class);
+        this.rabbitMqService = new RabbitMqServiceImp(this.rabbitTemplate);
+    }
+
+    @Test
+    void shouldSendMessageForProducerRabbitMqAndReturnException(){
+
+    }
+
+
 }

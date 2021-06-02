f82d01eb49275ff2bdd0c9b632a38fbca94dc4a1
Mauri <maurigre@gmail.com>
2021-06-02 17:58:35 -0300

    test(): add test unit construct methodo MessageMapper

--- src/test/java/br/com/magalu/labs/communication/controller/v1/dto/mapper/MessageMapperUnitTest.java
+++ src/test/java/br/com/magalu/labs/communication/controller/v1/dto/mapper/MessageMapperUnitTest.java
@@ -11,10 +11,16 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class MessageMapperUnitTest {
 
 
+    @Test
+    void shouldCallTheClassConstructorAndReturnNotNull(){
+        MessageMapper messageMapper = new MessageMapper();
+       assertNotNull(messageMapper);
+    }
 
     @Test
     void shouldReceiveMessageToMessageDto(){
